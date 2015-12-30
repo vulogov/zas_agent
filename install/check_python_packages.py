914de2c65daa53fd2550df40312265e9cfd1274c
vulogov <vladimir.ulogov@me.com>
2015-12-30 17:11:24 -0500

    ver 0.1.1
    Add Python packages verification script

---
+++ install/check_python_packages.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+PKGS=['os','sys','ConfigParser','argparse','struct','multiprocessing','socket','time','logging','redis','numpy','fnmatch','re','signal','daemonize']
+
+for p in PKGS:
+    import imp
+    m_repr = "'%s'"%p
+    m_repr = m_repr.ljust(20,".")
+    print "Module %s"%m_repr,
+    try:
+        fp, pathname, description = imp.find_module(p)
+        imp.load_module(p, fp, pathname, description)
+    except:
+        print "FAIL"
+        continue
+    finally:
+        print "OK"
