ec8a931ad87bcd7ed1ec717a78e2d052e25f88e6
Alexander Zobnin <alexanderzobnin@gmail.com>
2019-10-08 17:43:27 +0300

    allow to upgrade redis and simplejson versions

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     install_requires=[
         "daemonize == 2.4.2",
         "numpy == 1.16.5",
-        "redis == 2.0.0",
-        "simplejson == 2.0.9"
+        "redis >= 2.0.0",
+        "simplejson >= 2.0.9"
     ]
     )
